8af586429244f54fc15c55e9ba76e6ebfefcbf5e
Arnold-keyx <arnold2015@sina.com>
2017-03-21 11:27:45 +0800

    build

---
+++ app/components/loading.vue
@@ -0,0 +1,20 @@
+<template>
+    	<img class="loading" src="../assets/loading.gif">
+</template>
+
+<script> 
+ 
+</script>
+      
+<style lang="sass">
+    body{
+      background: #fff;
+    
+        .loading{
+
+    	       width: 100%;
+    	       margin:50% 0;
+             }
+        }
+   
+</style>
